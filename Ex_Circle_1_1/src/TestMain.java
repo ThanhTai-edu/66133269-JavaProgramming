4fdb3636129f1d14c1ba0167ac485b70551ccdb3
ThanhTai-edu <tai.tvt.66cntt@ntu.edu.vn>
2026-03-09 17:32:06 +0700

    bài tập 1.1

---
+++ Ex_Circle_1_1/src/TestMain.java
@@ -0,0 +1,13 @@
+
+public class TestMain {
+    public static void main(String[] args) {
+
+        Circle c1 = new Circle();
+        System.out.println("Radius: " + c1.getRadius());
+        System.out.println("Area: " + c1.getArea());
+
+        Circle c2 = new Circle(2.0);
+        System.out.println("Radius: " + c2.getRadius());
+        System.out.println("Area: " + c2.getArea());
+    }
+}
