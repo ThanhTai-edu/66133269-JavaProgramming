95adc718d31f51117c56d43d785c60c874bff5ab
ThanhTai-edu <tai.tvt.66cntt@ntu.edu.vn>
2026-03-10 19:08:11 +0700

    Ex_Employee_1_4

---
+++ Ex_Employee_1_4/src/Employee.java
@@ -0,0 +1,23 @@
+
+public class Employee {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int salary;
+
+    public Employee(int id, String firstName, String lastName, int salary) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return firstName + " " + lastName;
+    }
+
+    public int getAnnualSalary() {
+        return salary * 12;
+    }
+}
