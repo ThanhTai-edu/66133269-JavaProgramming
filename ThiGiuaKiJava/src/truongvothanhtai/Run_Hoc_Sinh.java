2edfc31d2a19cfc36f2385d4889f6ebdfb26848b
ThanhTai-edu <tai.tvt.66cntt@ntu.edu.vn>
2026-04-22 21:42:47 +0700

    BaiTap
    10 phut lan 1

--- ThiGiuaKiJava/src/truongvothanhtai/Run_Hoc_Sinh.java
+++ ThiGiuaKiJava/src/truongvothanhtai/Run_Hoc_Sinh.java
@@ -21,4 +21,46 @@ public class Run_Hoc_Sinh {
 //		System.out.println("Nhap lop hoc sinh ");
 //		hs3.setLopHS(sc.nextInt());
 //		hs3.hienthithongtin();
-}	
+		
+		ArrayList <HocSinh> ds = new ArrayList();
+		System.out.println("Nhap n hoc sinh ");
+		int n = sc.nextInt();
+		sc.nextLine();
+		
+		for(int i = 1 ; i <n ; i++) {
+			HocSinh hsi = new HocSinh();
+			System.out.println("Nhap ten hoc sinh thu "+i);
+			String ten = sc.nextLine();
+			hsi.setTenHS(ten);
+			System.out.println("Nhap tuoi hoc sinh"+i);
+			short tuoi = sc.nextShort();
+			hsi.setTuoiHS(tuoi);
+			System.out.println("Nhap lop hoc sinh thu "+i);
+			hsi.setLopHS(sc.nextInt());
+			sc.nextLine();
+			ds.add(hsi);
+		}
+		for(var hs : ds) {
+			hs.hienThiThongTin();
+		}
+		HocSinh hsthem = new HocSinh("Hoa",(short)12,12);
+		ds.add(hsthem);
+		for(var hs : ds) {
+			hs.hienThiThongTin();
+		}
+//		System.out.println("Nhap ten hoc sinh muon tim ");
+//		String tentim = sc.nextLine();
+		
+		for(int i = 0 ; i< ds.size(); i++) {
+			HocSinh hstim = ds.get(i);
+			if(hstim.getTenHS() .equals("Hoa")){
+				ds.remove(i);
+				break;
+			}
+		}
+		for(var hs : ds) {
+			hs.hienThiThongTin();
+		}
+	}
+
+}
